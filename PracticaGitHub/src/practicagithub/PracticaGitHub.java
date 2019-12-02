9131cd65ee77a5b75bf9cb028ef09564cc569308
Alvaro Gimenez <agimgue13@insgabrielamistral.cat>
2019-12-02 16:00:36 +0100

    funcio1-FETA

--- PracticaGitHub/src/practicagithub/PracticaGitHub.java
+++ PracticaGitHub/src/practicagithub/PracticaGitHub.java
@@ -17,18 +17,19 @@ public class PracticaGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-         Scanner sc = new Scanner(System.in);
-        int año;
+       Scanner sc = new Scanner(System.in);
+        int any;
         System.out.print("Introdueix l'any: ");
-        año = sc.nextInt();
-        if(anyBisiesto(año))  
-           System.out.println("L'any es Bisiesto");
+        any = sc.nextInt();
+        if(anyBisiesto(any))  
+           System.out.println("L'any es bisiesto");
         else
            System.out.println("L'any no es bisiesto");
-    }
+        
+    }   
     
-    public static boolean anyBisiesto(int a){   
+    public static boolean anyBisiesto(int a){       
+        
         if(a%4==0 && a%100!=0 || a%400==0)
            return true;
         else
